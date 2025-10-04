7de686cb7c5a43586477f5e9afc2216b4e6a599b
unknown <ajibond02@gmail.com>
2025-10-04 14:48:25 +0700

    tugas saya bagus nuswantoro

---
+++ Documents/vanila.js/project/1-mini-calculator.js
@@ -0,0 +1,33 @@
+let a=100;
+let b=65;
+let c=30;
+let d=20;
+let e=2;
+penjumlahan= (a, b, c, d,) =>  {
+    console.log(`jika a + b: ${a} + ${b}  =${a + b}`);
+}
+penjumlahan(a, b, c, d);
+
+pengurangan= (a, b, c, d) =>  {
+    console.log(`jika a - b: ${b} - ${c}  =${b - c}`);
+}
+pengurangan(a, b, c, d);
+
+perkalian= (a, b, c, d) =>  {
+    console.log(`jika a x b: ${d} x ${c}  =${d * c}`);
+}
+perkalian(a, b, c, d);
+
+pembagian= (a, b, c, d) =>  {
+    console.log(`jika a : b: ${a} : ${d}  =${a / d}`);
+}
+pembagian(a, b, c, d);
+perpangkatan= (a, b, c, d, e) =>  {
+    console.log(`jika a xx e: ${a} xx ${e} = ${a ** e}`);
+}
+perpangkatan(a, b, c, d, e);
+persenan= (a, b, c, d) =>  {
+    console.log(`jika b % a: ${b} % ${a}  =${b % a}`);
+}
+persenan(a, b, c, d);
+
